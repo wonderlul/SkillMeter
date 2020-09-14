a39574ab515c82c3ce8e152282d6ecb4d976c983
lulek92 <lulmichal@gmail.com>
2020-09-14 13:13:58 +0200

    [wip] removed comments

--- server/skills/models/skills.model.ts
+++ server/skills/models/skills.model.ts
@@ -15,14 +15,12 @@ export interface ISkills extends Document {
   name: string;
   category: string;
   weight: ESkills;
-  commnents?: string;
 }
 
 const SkillsSchema: Schema = new Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
   weight: { type: ESkills, required: true },
-  comments: { type: String, required: false },
 });
 
 export default mongoose.model<ISkills>("skills", SkillsSchema);
